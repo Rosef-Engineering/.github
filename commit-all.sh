e5e1bf55fbede45f77329770aebba6e3f77be516
Filip Cvejić <fcvejic@gmail.com>
2024-05-06 15:08:48 +0200

    added status commands and new line echo

--- commit-all.sh
+++ commit-all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+git status
 git commit -am "$1"
 git push
+echo $'\n'
 cd ../elv_socket-plug/kicad/
 git restore --staged .
 git add "README.md"
+git status
 git commit -m "$1"
 git push
 cd ../../github-doc
