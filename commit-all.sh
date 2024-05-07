aa53e6e330cdbc8a39b1619f332d07ba1fd07ac8
Filip Cvejić <fcvejic@gmail.com>
2024-05-07 18:54:25 +0200

    tried to make commit-all more userfriendly and idiot-proof

--- commit-all.sh
+++ commit-all.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
+[ $# -lt 1 ] && echo $'Error - no commit message\n' && exit 1
+
+git add .
 git status
-git commit -am "$1"
+
+read -n1 -p "Continue? [y/N] " reply
+echo ""
+[ "$reply" != "Y" ] && [ "$reply" != "y" ] && echo $'Aborting\n' && exit 1
+
+git commit -m "$1"
 git push
 
-echo $'\n\n'
+#####################
+# elv_socket/kicad/
+
+echo $'\n'
 cd ../elv_socket/kicad/
-git restore --staged .
+git status
+
+read -n1 -p "Continue? [y/N] " reply
+echo ""
+[ "$reply" != "Y" ] && [ "$reply" != "y" ] && echo $'Aborting\n' && exit 1
+
+git stash --staged .
 git add "README.md"
 git status
 git commit -m "$1"
 git push
+git pop
 cd ../../github-doc
 
-echo $'\n\n'
+#####################
+# reqs-arch/
+
+echo $'\n'
 cd ../reqs-arch/
-git restore --staged .
+git status
+
+read -n1 -p "Continue? [y/N] " reply
+echo ""
+[ "$reply" != "Y" ] && [ "$reply" != "y" ] && echo $'Aborting\n' && exit 1
+
+git stash --staged .
 git add "README.md"
 git status
 git commit -m "$1"
 git push
+git pop
 cd ../github-doc
